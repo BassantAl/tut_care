a256f4cd8cf069a1865556e8f0c35f365a850a4d
bassantAli <basant.ali123zx@gmail.com>
2026-07-31 12:58:10 +0300

    edit  AppointmentsRepoImpl

--- lib/features/Appointments/data/repos/appointments_repo_impl.dart
+++ lib/features/Appointments/data/repos/appointments_repo_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:tut_care/core/errors/failure.dart';
+import 'package:tut_care/core/errors/server_errors.dart';
 import 'package:tut_care/core/services/api_service.dart';
 import 'package:tut_care/features/Appointments/data/models/my_appointment_model.dart';
 import 'package:tut_care/features/Appointments/data/repos/appointments_repo.dart';
@@ -9,13 +10,15 @@ class AppointmentsRepoImpl implements AppointmentsRepo {
   AppointmentsRepoImpl({required this.apiService});
   final ApiService apiService;
   @override
-  Future<Either<Failure, MyAppointmentModel>> getMYAppointments() async{
-    try{
-    var result =await   apiService.get(url: '/api/Patient/MyAppointments');
-    MyAppointmentModel data=MyAppointmentModel.fromJson(result.data);
-    return right(data);
-    }on DioException catch(e){
-      re
+  Future<Either<Failure, MyAppointmentModel>> getMYAppointments() async {
+    try {
+      var result = await apiService.get(url: '/api/Patient/MyAppointments');
+      MyAppointmentModel data = MyAppointmentModel.fromJson(result.data[0]);
+      return right(data);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(e));
+    } catch(e){
+       return left(Failure(errorMessage: e.toString()));
     }
   }
 }
